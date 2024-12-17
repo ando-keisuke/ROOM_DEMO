81674091bc5dd863bddcdfaf4e86ece231342c4c
ANDO_KEISUKE <it242017@trident.ac.jp>
2024-12-18 03:19:44 +0900

    コメントを追加

--- app/src/main/java/com/example/room_demo/UserDao.java
+++ app/src/main/java/com/example/room_demo/UserDao.java
@@ -12,19 +12,28 @@ import java.util.List;
 // @Daoアノテーションをクラスの前につける
 @Dao
 public interface UserDao {
+    // すべてのユーザーを取得するクエリ
     @Query("SELECT * FROM user")
     List<User> getAll();
 
+    // uidが一致するユーザーをすべて取得するクエリ
     @Query("SELECT * FROM user WHERE uid IN (:userIds)")
     List<User> loadAllByIds(int[] userIds);
 
+    // uidが一致するユーザーを取得するクエリ
+    @Query("SELECT * FROM User WHERE uid = (:uid)")
+    User findById(int uid);
+
+    // first_nameとlast_nameが一致するユーザーを取得するクエリ
     @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
             "last_name LIKE :last LIMIT 1")
     User findByName(String first, String last);
 
+    // ユーザーを追加する
     @Insert
     void insertAll(User... users);
 
+    // ユーザーを削除する
     @Delete
     void delete(User user);
 }
